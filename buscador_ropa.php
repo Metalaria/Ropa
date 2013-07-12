052353fc9b4568fcfcd98bb7c515a4f030b82b88
metalaria <gonzalo.mejias.moreno@gmail.com>
2013-07-12 12:43:00 +0200

    change the file to adapt it to the new project

---
+++ buscador_ropa.php
@@ -0,0 +1,24 @@
+<?php
+
+session_start();
+
+        if (!isset($_SESSION['id_user'])) {
+            echo "No ha iniciado sesi&oacute;n";
+            header("refresh: 3; url= index.php");   
+        }else {
+        echo "<form action='busqueda.php' method='post' enctype='multipart/form-data'>
+                Id de la prenda:<input type='text' name='busqueda' value='' /><br/>
+                Tipo de prenda:<input type='text' name='busqueda' value='' /><br/>
+                <input type='submit' name='boton' value='buscar' />
+        </form>";
+        
+        echo "<form action='buscar_todos.php' method='post' enctype='multipart/form-data'>
+            
+            <input type='submit' name='todos' value='Todas las prendas' /><br/>
+            
+        </form>";
+        }
+         ?>
+        <a href='exito.php'>volver atr&aacute;s</a>
+
+?>
