a27c36c0de3f114a4eb51dd2c084f87897f5d5e0
metalaria <gonzalo.mejias.moreno@gmail.com>
2013-07-22 10:26:34 +0200

    fix action

--- cliente.php
+++ cliente.php
@@ -5,7 +5,7 @@ session_start();
             echo "No ha iniciado sesi&oacute;n";
             header("refresh: 3; url= index.php");   
         } else{
-            echo "<p><a href='registrar.php'>a&ntilde;adir una prenda</a></p>
+            echo "<p><a href='registrar_ropa.php'>a&ntilde;adir una prenda</a></p>
                 <p><a href='buscar.php'>Modificar una prenda</a></p>
                 <p><a href='buscador_ropa.php'>Buscar una prenda</a></p>
                 <p><a href='eliminador_ropa.php'>Eliminar una prenda</a></p>
