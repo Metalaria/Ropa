c524b982191b3270e3da0ca764c5918cdc61262f
metalaria <gonzalo.mejias.moreno@gmail.com>
2013-08-01 10:39:07 +0200

    create new file

---
+++ buscar_todos_ropa.php
@@ -0,0 +1,24 @@
+<?php
+
+
+session_start();
+if (!isset($_SESSION['id_user'])) {
+            echo "No ha iniciado sesi&oacute;n";
+            header("refresh: 3; url= index.php");   
+        } else{
+
+$con = new mysqli('localhost', 'root', '', 'ropa');
+$busca = mysqli_query($con, "select id_prendas, pren_usuario, pren_codigo, pren_tipo
+    from usuarios ");
+    
+    while ($f =  mysqli_fetch_array($busca)){
+        echo $f ['id_prendas']. '&nbsp;'. '&nbsp;'.$f['pren_usuario'].
+                '&nbsp;'. '&nbsp;'.$f['pren_codigo'].
+                '&nbsp;'. '&nbsp;'.$f['pren_tipo']."<br/>" ; 
+    }
+    echo "<form name= 'form1' action='atras_cliente.php' method='POST' >
+            <input type='submit' name='regresar' value='regresar' />
+        </form>";
+        }
+
+?>
