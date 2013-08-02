19ad62922e71d9575f5fbaf031c6c0ec2869cb47
metalaria <gonzalo.mejias.moreno@gmail.com>
2013-08-02 11:01:48 +0200

    modify file for inserting in two tables and insert the foreign key. Added also a function to generate the id of the event

--- insertar_ropa.php
+++ insertar_ropa.php
@@ -10,11 +10,30 @@ $cod = $_POST['cod'];
 $tipo = $_POST['tipo'];
 $user = $_SESSION['id_user'];
 
-                  mysqli_query($con,"insert into prendas 
-                      (pren_usuario,pren_codigo,pren_tipo)
+function create_event($var){
+(int)$var++;
+
+for($i = 0; $i <=$var; $i++) {
+    $event = $var;
+return $event;
+}
+}
+$execute = mysqli_query($con, "select event_identificador from eventos
+    WHERE event_identificador=(SELECT MAX(event_identificador) FROM eventos)");
+$row = mysqli_fetch_array($execute);
+$var1 = $row['event_identificador'];
+
+$evento = create_event($var1);
+                  mysqli_query($con,"insert into eventos 
+                      (event_identificador, event_descripcion)
                       values
-                      ('$user','$cod','$tipo')")  ;
+                      ($evento, 'Prenda insertada')")  ;
+                  mysqli_query($con,"insert into prendas 
+                      (pren_usuario,pren_codigo,pren_tipo, pren_evento)
+                      values
+                      ('$user','$cod','$tipo', $evento)")  ;
                   
+                  echo "Prenda insertada con &eacute;xito";
 	  	}
          
 header('refresh: 3; url= cliente.php'); 
